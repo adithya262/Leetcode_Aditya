d76783e3a639dbab940e87ff9a32ba47734c4152
_Aditya_2603 <adithyarbi@gmail.com>
2025-07-14 13:45:32 +0530

    Time: 0 ms (100%), Space: 17.7 MB (78.78%) - LeetHub

--- 0014-longest-common-prefix/0014-longest-common-prefix.py
+++ 0014-longest-common-prefix/0014-longest-common-prefix.py
@@ -3,9 +3,21 @@ class Solution:
         if not strs:
             return ""
 
-        for i in range(len(min(strs, key=len))):  
-            char = strs[0][i]  
-            for s in strs[1:]:
-                if s[i] != char:
-                    return strs[0][:i]
-        return strs[0][:len(min(strs, key=len))]
+        min_length=float('inf')
+
+        for s in strs:
+            if len(s) <min_length:
+                min_length=len(s)
+
+        i=0
+        while i <min_length:
+            for s in strs:
+                if s[i] != strs[0][i]:
+                    return s[:i]
+            i+=1
+
+        return strs[0][:i]
+
+
+
+
