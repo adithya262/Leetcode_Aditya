18d98cb78ed56c907c4caa79c3024438369b13d3
_Aditya_2603 <adithyarbi@gmail.com>
2025-07-24 20:55:09 +0530

    Time: 1 ms (14.39%), Space: 17.8 MB (65.11%) - LeetHub

---
+++ 0078-subsets/0078-subsets.py
@@ -0,0 +1,46 @@
+class Solution:
+
+    def subsets(self, nums: List[int]) -> List[List[int]]:
+
+        n = len(nums)
+
+        ans, sol = [], []
+
+    
+
+        def backtrack(i):
+
+            if i == n:
+
+                ans.append(sol[:])
+
+                return
+
+    
+
+            # Don't pick nums[i]
+
+            backtrack(i + 1)
+
+    
+
+            # Pick nums[i]
+
+            sol.append(nums[i])
+
+            backtrack(i + 1)
+
+            sol.pop()
+
+    
+
+        backtrack(0)
+
+        return ans
+
+    
+
+# Time Complexity: O(2^n)
+
+# Space Complexity: O(n)
+
